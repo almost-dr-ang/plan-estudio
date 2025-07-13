b460a73ff1d18397d4bfdf8cca048f978d502138
almost-dr-ang <mariadelapena2104@gmail.com>
2025-07-13 18:14:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function toggleRamo(ramoID) {
+  const checkbox = document.querySelector(`#${ramoID} input[type="checkbox"]`);
+  const aprobado = checkbox.checked;
+
+  // Revisar todos los ramos para ver si deben desbloquearse
+  document.querySelectorAll('.ramo').forEach(div => {
+    const prereqs = div.dataset.prereqs;
+    if (!prereqs) return;
+
+    const prereqArray = prereqs.split(',');
+    const fulfilled = prereqArray.every(id => {
+      const cb = document.querySelector(`#${id} input[type="checkbox"]`);
+      return cb && cb.checked;
+    });
+
+    const input = div.querySelector('input[type="checkbox"]');
+    input.disabled = !fulfilled;
+  });
+}
 
